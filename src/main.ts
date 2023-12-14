e497ca9c647e972bbf38f119f9b0dcfab233a206
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-12-14 12:24:18 -0300

    dd

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { IoAdapter } from '@nestjs/platform-socket.io';
-import * as socketio from 'socket.io';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
